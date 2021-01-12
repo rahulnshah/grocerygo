2a076b34cf76576cc330521aadce8a536f3f88e5
Rahul Naishadkumar Shah <68120349+rshah33@users.noreply.github.com>
2021-01-12 17:19:36 -0500

    Update emailInPython.py

--- emailInPython.py
+++ emailInPython.py
@@ -2,15 +2,16 @@ import smtplib
 from email.message import EmailMessage
 
 
-def email_content(body, s_address = "rahulnshah3012@gmail.com", r_address = "rahulnshah3012@gmail.com", subject_line = ""):
+def email_content(body, s_address = "********", r_address = "*******", subject_line = ""):
     msg = EmailMessage()
     msg['Subject'] = subject_line
     msg['From'] = s_address
     msg['To'] = r_address
     msg.set_content(body)
     with smtplib.SMTP_SSL('smtp.gmail.com' , 465) as smtp:
-        # login in gmail account from python- you have to be logged in to make this work
-        smtp.login("rahulnshah3012", "********")
+        # login in gmail account from python- you have to be logged in through python for this to work- get your own password by setting up 2-Step Verfication 
+        # on your gmail account.  Below, first put your email address, then the, your 16-letter password for python (no spaces)
+        smtp.login("********", "********")
         
         smtp.send_message(msg)
         
