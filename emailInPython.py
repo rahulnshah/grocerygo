cf71b6b9e87cfb6c4e7794317bc32290165804e2
Rahul Shah <rahulnshah3012@gmail.com>
2021-01-12 17:13:16 -0500

    New Background and Email List button with functionality added

--- emailInPython.py
+++ emailInPython.py
@@ -10,7 +10,7 @@ def email_content(body, s_address = "rahulnshah3012@gmail.com", r_address = "rah
     msg.set_content(body)
     with smtplib.SMTP_SSL('smtp.gmail.com' , 465) as smtp:
         # login in gmail account from python- you have to be logged in to make this work
-        smtp.login("rahulnshah3012", "vibmnjbevvzrbptk")
+        smtp.login("rahulnshah3012", "********")
         
         smtp.send_message(msg)
         
