097d86bbd34af29e1cc75054f20262b8864f286c
Rahul Shah <rahulnshah3012@gmail.com>
2024-08-08 22:26:44 -0400

    routes done; GET working

--- app/api/items/count/[listId]/route.ts
+++ app/api/items/count/[listId]/route.ts
@@ -5,10 +5,18 @@ import { sql } from '@vercel/postgres';
 export async function GET(req: Request, { params }: { params: { listId: string } }) {
   const { listId } = params;
   try {
-    const { rows } = await sql`
-      SELECT * FROM favorites WHERE list_id = ${listId}
+    const {rows} = await sql`
+      SELECT 
+        COUNT(*) AS items_count
+      FROM 
+        items
+      WHERE 
+        list_id = ${listId}
+      GROUP BY 
+        list_id
     `;
-    return NextResponse.json(rows);
+    const num = rows[0]?.items_count || "0";
+    return NextResponse.json(num);
   } catch (error) {
     console.error(`Error fetching favorite lists for list ID ${listId}:`, error);
     return NextResponse.error();
---
+++ routes.md
@@ -0,0 +1,9 @@
+- GET `/api/lists` to get all lists.
+- POST `/api/lists` to create a new list.
+- GET `/api/lists/[id]` to get a specific list by ID.
+- PUT `/api/lists/[id]` to update a specific list by ID.
+- DELETE `/api/lists/[id]` to delete a specific list by ID.
+- Get All Favorite Lists: `app/api/favorites/route.ts`
+- Add a Favorite List:` app/api/favorites/[listId]/route.ts`
+- Get Favorite Lists by List ID: `app/api/favorites/list/[listId]/route.ts`
+- Remove a Favorite List: `app/api/favorites/[id]/route.ts`
