b324014afa277ca777cce28f72057c9690c3080d
Rahul Shah <rahulnshah3012@gmail.com>
2024-12-29 18:08:30 -0500

    made UI look nice, fixed copy list

--- app/notebook/layout.tsx
+++ app/notebook/layout.tsx
@@ -1,10 +1,14 @@
 import { ReactNode } from 'react';
 import Navbar from '../ui/notebook/NavBar';
+import { auth } from '@/auth';
 
-const NotebookLayout = ({ children }: { children: ReactNode }) => {
+const NotebookLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+  if (!session?.user) return null;
+  const owner_name = session?.user?.name || '';
   return (
     <div className="min-h-screen">
-      <Navbar />
+      <Navbar username={owner_name} />
       <div className="container mx-auto px-4">
         <div className="pt-8">{children}</div>
       </div>
--- app/ui/notebook/NavBar.tsx
+++ app/ui/notebook/NavBar.tsx
@@ -1,23 +1,34 @@
 import { signOut } from "@/auth";
 import AllLinks from "./AllLinks";
-const Navbar = () => {
+
+interface NavbarProps {
+  username: string;
+}
+
+const Navbar = ({ username }: NavbarProps) => {
   return (
-    <nav className="bg-[#5B3A29]">
+    <nav className="bg-[#6B4423]">
       <div className="flex items-center justify-between p-4">
         <div className="flex items-center space-x-4">
           <AllLinks/>
         </div>
-        <form action={async () => {
-          'use server';
-          await signOut();
-        }}>
-          <button
-            type="submit"
-            className="button-primary"
-          >
-            Log out
-          </button>
-        </form>
+        
+        <div className="flex items-center space-x-4">
+          <h1 className="text-white font-semibold">
+            Hello there, {username}!
+          </h1>
+          <form action={async () => {
+            'use server';
+            await signOut();
+          }}>
+            <button
+              type="submit"
+              className="button-primary"
+            >
+              Log out
+            </button>
+          </form>
+        </div>
       </div>
     </nav>
   );
